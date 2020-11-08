fc2e9ea619ca98ba72fff51c98a1f4d60ffab9d2
Alister Burt <alisterburt@gmail.com>
2020-11-08 21:12:56 +0100

    add dynamo helper functions and euler2matrix function for relion in relion helper

---
+++ peepingtom/utils/constants/dynamo_constants.py
@@ -0,0 +1,3 @@
+dynamo_table_coordinate_headings = ['x', 'y', 'z']
+dynamo_table_shift_headings = ['dx', 'dy', 'dz']
+dynamo_euler_angle_headings = ['tdrot', 'tilt', 'narot']
--- peepingtom/utils/relion_helper.py
+++ peepingtom/utils/relion_helper.py
@@ -1,11 +1,12 @@
-from pandas import DataFrame
+import numpy as np
+import pandas as pd
 from eulerangles import euler2matrix
 
 from .validators import columns_in_df
 from .constants.relion_constants import relion_coordinate_headings_3d, relion_shift_headings_3d, relion_euler_angle_headings
 
 
-def df_to_xyz(df: DataFrame):
+def df_to_xyz(df: pd.DataFrame):
     """
 
     Parameters
@@ -27,7 +28,7 @@ def df_to_xyz(df: DataFrame):
     return positions.to_numpy()
 
 
-def df_to_euler_angles(df: DataFrame):
+def df_to_euler_angles(df: pd.DataFrame):
     """
 
     Parameters
@@ -43,7 +44,21 @@ def df_to_euler_angles(df: DataFrame):
     return euler_angles.to_numpy()
 
 
-def df_to_rotation_matrices(df: DataFrame):
+def euler_angles_to_rotation_matrices(euler_angles: np.ndarray):
+    """
+
+    Parameters
+    ----------
+    euler_angles : (n, 3) ndarray or RELION euler angles
+
+    Returns (n, 3, 3) ndarray of rotation matrices which premultiply column vectors [x, y, z]
+    -------
+
+    """
+    return euler2matrix(euler_angles, axes='zyz', intrinsic=True, positive_ccw=True)
+
+
+def df_to_rotation_matrices(df: pd.DataFrame):
     """
 
     Parameters
@@ -55,5 +70,5 @@ def df_to_rotation_matrices(df: DataFrame):
 
     """
     euler_angles = df_to_euler_angles(df)
-    rotation_matrices = euler2matrix(euler_angles, axes='zyz', intrinsic=True, positive_ccw=True)
+    rotation_matrices = euler_angles_to_rotation_matrices(euler_angles)
     return rotation_matrices
