99d00d539167066a45201fcaef29f586fa8325ba
Alister Burt <alisterburt@gmail.com>
2020-11-08 20:48:41 +0100

    add validators.py module to hold simple validator functions

---
+++ peepingtom/utils/validators.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def columns_in_df(columns: list, df: pd.DataFrame):
+    """
+    Checks that a list of columns are all present in a given DataFrame
+    Parameters
+    ----------
+    columns : list of column names to check
+    df : DataFrame to be checked
+
+    Returns bool
+    -------
+
+    """
+    return pd.Series(columns).isin(df.columns).all()
+
