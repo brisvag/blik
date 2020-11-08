912bc86aafd4d42c1f3a4fb5d6a56411e22fc7d8
Alister Burt <alisterburt@gmail.com>
2020-11-08 16:08:58 +0100

    add PointBlock object

--- peepingtom/base/datablock.py
+++ peepingtom/base/datablock.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
 import pandas as pd
+from scipy.interpolate import splprep, splev
+
 from ..utils.components import Child
 
 
@@ -37,11 +40,197 @@ class DataBlock(Child, ABC):
         return self.data[item]
 
 
+class PointBlock(DataBlock):
+    """
+    PointBlock objects for representing points with convenience methods
 
+    PointBlock data should be array-like objects of shape (n, m) representing n points in m spatial dimensions
 
-class Points(DataBlock):
+    order of dimensions along m is:
+    2d : (x, y)
+    3d : (x. y, z)
+    nd : (..., x, y, z)
+    """
+
     def __init__(self, points, **kwargs):
         super().__init__(**kwargs)
+        self.data = points
+
+    def _data_setter(self, points):
+        # cast as array
+        points = np.asarray(points)
+
+        # coerce 1d point to 2d
+        if points.ndim == 1:
+            points.reshape((1, len(points)))
+
+        # check ndim of points
+        assert points.ndim == 2
+
+        self._data = points
+
+    @property
+    def ndim_spatial(self):
+        return self.data.shape[1]
+
+    def _named_dimension_to_spatial_index(self, dim: str):
+        """
+        Gets the index of the named dimension 'x', 'y' or 'z'
+        Parameters
+        ----------
+        dim : str, must be one of 'x', 'y' or 'z'
+
+        Returns data along named dimension
+        -------
+
+        """
+        # sanitise input
+        dim = str(dim.strip().lower())
+
+        # dim to index for 3d or less
+        dim_to_index = {'x': 0,
+                        'y': 1,
+                        'z': 2}
+
+        dim_idx = dim_to_index[dim]
+
+        # check and correct index for higher dimensionality
+        if self.ndim_spatial >= 3:
+            dim_idx = -dim_idx - 1
+
+        return dim_idx
+
+    def _get_dim_at_spatial_index(self, idx: int):
+        return self[:, idx]
+
+    def _get_named_dimension(self, dim: str, as_array=None, as_tuple=None):
+        """
+        Get data for a named dimension or multiple named dimensions of the object
+
+        as_array and as_tuple are only considered when retrieving multiple dimensions in one method call
+        Parameters
+        ----------
+
+        dim : str 'x', 'y', 'z' or a combination thereof
+        as_array : bool, force return type to be ndarray (incompatible with as_tuple)
+        as_tuple : bool, force return type to be tuple (incompatible with as_array)
+
+        Returns : (default) (n,m) ndarray of data along named dimension(s) from m
+                  or tuple of arrays of data along each axis
+        -------
+
+        """
+        if len(dim) > 1:
+            # split dims up and get each separately
+            data = [self._get_named_dimension(_dim) for _dim in dim]
+
+            # decide on output type and return array or tuple as requested, default to array
+            if  (as_array and not as_tuple) or (not (as_array and as_tuple)):
+                return np.column_stack(data)
+            elif as_tuple and not as_array:
+                return tuple(data)
+            else:
+                raise ValueError(f"'as_tuple' and 'as_array' cannot both be True")
+
+        else:
+            # get index of named dimension along spatial axis
+            dim_idx = self._named_dimension_to_spatial_index(dim)
+            # index into self to get data
+            return self._get_dim_at_spatial_index(dim_idx)
+
+    @property
+    def x(self):
+        return self._get_named_dimension('x')
+
+    @property
+    def y(self):
+        return self._get_named_dimension('y')
+
+    @property
+    def y(self):
+        return self._get_named_dimension('z')
+
+    @property
+    def xyz(self):
+        return self._get_named_dimension('xyz')
+
+    @property
+    def zyx(self):
+        return self._get_named_dimension('zyx')
+
+    @property
+    def center_of_mass(self):
+        return np.mean(self.data, axis=0)
+
+    def distance_to(self, point):
+        """
+        Calculate the euclidean distance between the center of mass of this object and a point
+
+        Parameters
+        ----------
+        point : array-like object
+
+        Returns : euclidean distance
+        -------
+
+        """
+        point = np.asarray(point)
+        assert point.shape == self.center_of_mass.shape
+        return np.linalg.norm(point - self.center_of_mass)
+
+
+class LineBlock(DataBlock, Points):
+    """
+    Line objects with convenience methods
+
+    Line data should be array-like objects of shape (n, m) representing n ordered points in m spatial dimensions
+
+    order of dimensions along m is (x, y, z)
+
+    Polarity (direction) of lines, lines start from 0 to n along the 0th dimension
+    """
+
+    def __init__(self, line, **kwargs):
+        """
+
+        Parameters
+        ----------
+        line : array-like objects of shape (n, m) representing n ordered points in m spatial dimensions
+        kwargs : kwargs are passed to DataBlock object
+
+        """
+        super(DataBlock).__init__(**kwargs)
+        super(Points).__init__(points=line)
+
+        # attributes related to spline fitting
+        self.spline_smoothing_parameter = 0
+        self._tck = None
+
+    @property
+    def spline_smoothing_parameter(self):
+        return self._spline_smoothing_parameter
+
+    @spline_smoothing_parameter.setter
+    def spline_smoothing_parameter(self, value):
+        self._spline_smoothing_parameter = float(value)
+
+    def fit_spline(self):
+        self._tck = splprep(self.dims_list, self.spline_smoothing_parameter)
+        return self._tck
+
+    def evaluate_spline(self, n_points):
+        u = np.linspace(0, 1, n_points, endpoint=True)
+        return splev(u, tck=self._tck)
+
+    @property
+    def smooth_backbone(self):
+        return self._generate_smooth_backbone()
+
+    def _generate_smooth_backbone(self, n_points=1000):
+        u = np.linspace(0, 1, n_points, endpoint=True)
+        self.fit_spline()
+        return self.evaluate_spline(n_points)
+
 
 class Particles(DataBlock):
     """
