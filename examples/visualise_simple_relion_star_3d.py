14949f4e7e1e796cee108a315e59f014ceb48a1e
Alister Burt <alisterburt@gmail.com>
2020-11-22 20:46:57 +0100

    UNDERSTOOD EULERS!!!!!

--- examples/visualise_simple_relion_star_3d.py
+++
@@ -1,6 +0,0 @@
-from peepingtom import data_star_to_crate, ParticlePeeper
-
-star_file = 'example_data/relion_3d_simple.star'
-p = ParticlePeeper(star_file)
-p.peep()
-
--- peepingtom/utils/helpers/dataframe_helper.py
+++ peepingtom/utils/helpers/dataframe_helper.py
@@ -83,7 +83,7 @@ def euler_angles_to_rotation_matrices(euler_angles: np.ndarray, mode: str):
     """
     euler_kwargs = {
         'relion': {'axes': 'zyz', 'intrinsic': True, 'positive_ccw': True},
-        'dynamo': {'axes': 'zxz', 'intrinsic': True, 'positive_ccw': True}
+        'dynamo': {'axes': 'zxz', 'extrinsic': True, 'positive_ccw': True}
     }
     _check_mode(mode)
     return euler2matrix(euler_angles, **euler_kwargs[mode])
@@ -96,9 +96,10 @@ def df_to_rotation_matrices(df: pd.DataFrame, mode: str):
     ----------
     df : RELION format STAR file as DataFrame (usually the result of starfile.read)
 
-    Returns : (n, 3, 3) ndarray of rotation matrices which premultiply column vectors [x, y, z]
+    Returns
     -------
-
+    rotation_matrices: (n, 3, 3) ndarray of float
+                       rotation matrices which premultiply column vectors [x, y, z]
     """
     euler_angles = df_to_euler_angles(df, mode)
     rotation_matrices = euler_angles_to_rotation_matrices(euler_angles, mode)
