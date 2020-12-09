a79e10b4ad749b04c5d2a3e5956b0c65cd296e30
Lorenzo Gaifas <brisvag@gmail.com>
2020-12-09 13:22:15 +0100

    updated init

--- peepingtom/__init__.py
+++ peepingtom/__init__.py
@@ -1,2 +1,2 @@
-from .io_ import star_to_crates, mrc_to_crates
-from .visualisation import Peeper, ParticlePeeper, peep
+from .io_ import read, build
+from .visualisation import peep, Peeper, ParticlePeeper
