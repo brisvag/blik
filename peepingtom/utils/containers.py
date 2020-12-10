883e095d1c288d081081afb7438ae6d0277dfdb4
Lorenzo Gaifas <brisvag@gmail.com>
2020-12-10 16:45:04 +0100

    fixed something, somehow

--- peepingtom/utils/containers.py
+++ peepingtom/utils/containers.py
@@ -36,24 +36,25 @@ class AttributedList(list):
         # check first in the normal way
         if hasattr(list(self), name):
             list.__setattr__(self, name, value)
+
         # can only safely do this recursively if we know where to find the atttributes lower down
         elif hasattr(self, name):
-            values = None
+            zip_values = None
+
             # if another Attributed list is fed, do it value by value if possible
             if isinstance(value, AttributedList):
                 # len(self) is not enough because it might be nested
                 if len(self.__getattribute__(name)) == len(value):
-                    values = list(reversed(value))
+                    zip_values = list(reversed(value))  # reversed for popping
+
             for item in self:
-                if values is not None:
-                    value = values.pop()
+                if zip_values is not None:
+                    # if an attributed list was given, use it value by value
+                    value = zip_values.pop()
                 # this lets us go as deep as needed, if working with nested lists
                 if isinstance(item, Iterable):
                     for sub_item in AttributedList(item):
                         sub_item.__setattr__(name, value)
-                if values is not None:
-                    if not values:
-                        raise AttributeError('attempted to set an AttributedList to an AttributedList of different length')
                 else:
                     item.__setattr__(name, value)
         else:
