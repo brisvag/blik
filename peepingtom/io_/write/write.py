6f49b8257bfd42fa6771af060990e2e94e68f049
Lorenzo Gaifas <brisvag@gmail.com>
2020-12-10 14:34:51 +0100

    added write functions for mrc and star. Still missing high level write logic

--- peepingtom/io_/write/write.py
+++ peepingtom/io_/write/write.py
@@ -1,6 +1,8 @@
-"""
-write functions
-"""
+from .mrc import write_mrc
+from .star import write_star
 
-def write():
-    pass
+
+def write(datablocks, paths, **kwargs):
+    """
+    wite a list of datablocks to disk
+    """
