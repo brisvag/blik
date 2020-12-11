6b884350457f48f376990b1addc81b95a063e00c
Alister Burt <alisterburt@gmail.com>
2020-12-11 19:56:08 +0100

    added CLI to the peep function

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+from peepingtom import __version__
+
+setup(
+    name='peepingtom',
+    version=f'{__version__}',
+    entry_points='''
+        [console_scripts]
+        peep=peepingtom.entry_points.peep:cli
+    '''
+)
