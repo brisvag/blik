74f14353c4c48d8f589d5e0023e01429af71bef6
Alister Burt <alisterburt@gmail.com>
2020-12-14 17:14:00 +0100

    move entry points to setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-from peepingtom import __version__
-
-setup(
-    name='peepingtom',
-    version=f'{__version__}',
-    entry_points='''
-        [console_scripts]
-        peep=peepingtom.entry_points.peep:cli
-    '''
-)
+setup()
