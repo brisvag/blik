0519d3b42e46fd9509df4dc0c075590bfa6008d1
Lorenzo Gaifas <brisvag@gmail.com>
2020-12-15 15:52:29 +0100

    particleblock now separates properties and metadata

--- peepingtom/core/datablocks/particleblock.py
+++ peepingtom/core/datablocks/particleblock.py
@@ -1,16 +1,19 @@
-import numpy as np
-
 from .orientedpointblock import OrientedPointBlock
 from .propertyblock import PropertyBlock
 
 
 class ParticleBlock(OrientedPointBlock):
-    def __init__(self, positions: np.ndarray, orientations: np.ndarray, properties: dict, **kwargs):
+    def __init__(self, positions, orientations, properties=None, metadata=None, **kwargs):
         # Initialise OrientedPointBlock
         super().__init__(positions, orientations, **kwargs)
 
         # Add PropertyBlock
+        if properties is None:
+            properties = {}
         self.properties = PropertyBlock(properties)
+        if metadata is None:
+            metadata = {}
+        self.metadata = {}
 
     def __shape_repr__(self):
         return f'{self.positions.data.shape}'
