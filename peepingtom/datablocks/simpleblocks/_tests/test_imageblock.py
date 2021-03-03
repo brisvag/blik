271f2c31fb40b6c7267366cb61e6f67396071047
Lorenzo Gaifas <brisvag@gmail.com>
2021-03-03 11:20:45 +0100

    some more tests

--- peepingtom/datablocks/simpleblocks/_tests/test_imageblock.py
+++ peepingtom/datablocks/simpleblocks/_tests/test_imageblock.py
@@ -1,8 +1,16 @@
 import numpy as np
 from peepingtom.datablocks.simpleblocks.imageblock import ImageBlock
+from peepingtom.depictors import ImageDepictor
 
 
 def test_imageblock_instantiation():
     im = np.random.normal(0, 1, (28, 28, 28))
     block = ImageBlock(im)
     assert isinstance(block, ImageBlock)
+
+
+def test_depiction():
+    ib = ImageBlock(np.zeros((3, 3, 3)))
+    ib.depict()
+    assert isinstance(ib.depictors[0], ImageDepictor)
+    ib.update()
