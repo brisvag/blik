7972bf17ce490fee44972f1035c4c904ed9ffb14
Lorenzo Gaifas <brisvag@gmail.com>
2021-03-03 10:30:03 +0100

    more coverage

--- peepingtom/alchemists/_tests/test_frompoints.py
+++ peepingtom/alchemists/_tests/test_frompoints.py
@@ -11,9 +11,11 @@ def test_point_to_line():
     p2l = PointToLineAlchemist(pb)
     assert isinstance(p2l.outputs[0], LineBlock)
     assert p2l.outputs[0].data is pb.data
+    p2l.update()
 
 
 def test_point_to_particle():
     p2p = PointToParticleAlchemist(pb)
     assert isinstance(p2p.outputs[0], ParticleBlock)
     assert p2p.outputs[0].positions.data is pb.data
+    p2p.update()
