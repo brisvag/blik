started migration to xarray where useful. Added parsing for pixel scale and automatic scaling of particles and images. Fixed some empty datablocks bugs.

peepingtom/datablocks/simpleblocks/imageblock.py
@@ -1,4 +1,5 @@
 import numpy as np
+from xarray import DataArray
 
 from .simpleblock import SimpleBlock
 from ...depictors import ImageDepictor
@@ -16,18 +17,19 @@ class ImageBlock(SimpleBlock):
         self.pixel_size = pixel_size
 
     def _data_setter(self, data):
-        return np.array(data)
+        data = np.array(data)
+        if data.ndim < 2:
+            raise ValueError('images must have at least 2 dimensions')
+        dims = ('z', 'y', 'x')
+        return DataArray(data, dims=dims[-data.ndim:])
 
     @property
-    def pixel_size(self):
-        return self._pixel_size
-
-    @pixel_size.setter
-    def pixel_size(self, value):
-        self._pixel_size = float(value)
-
     def ndim(self):
         return self.data.ndim
+
+    @property
+    def dims(self):
+        return self.data.dims
 
     def __shape_repr__(self):
         return f'{self.data.shape}'

peepingtom/datablocks/simpleblocks/pointblock.py
@@ -1,4 +1,5 @@
 import numpy as np
+from xarray import DataArray
 
 from .simpleblock import SimpleBlock
 from ...depictors import PointDepictor
@@ -17,7 +18,13 @@ class PointBlock(SimpleBlock):
     """
     _depiction_modes = {'default': PointDepictor}
 
+    def __init__(self, data=(), pixel_size=1, **kwargs):
+        super().__init__(data, **kwargs)
+        self.pixel_size = pixel_size
+
     def _data_setter(self, data):
+        if isinstance(data, DataArray):
+            return data
         # cast as array
         data = np.asarray(data)
 
@@ -31,7 +38,8 @@ class PointBlock(SimpleBlock):
         if not data.ndim == 2:
             raise ValueError("data object should have ndim == 2")
 
-        return data
+        dims = ['x', 'y', 'z']
+        return DataArray(data, dims=['n', 'spatial'], coords=(range(len(data)), dims[:data.shape[1]]))
 
     @property
     def ndim(self):
@@ -41,37 +49,11 @@ class PointBlock(SimpleBlock):
         """
         return self.data.shape[1]
 
-    def _named_dimension_to_spatial_index(self, dim: str):
-        """
-        Gets the index of the named dimension 'x', 'y' or 'z'
-        Parameters
-        ----------
-        dim : str, must be one of 'x', 'y' or 'z'
+    @property
+    def dims(self):
+        return tuple(self.data.spatial.data)
 
-        Returns data along named dimension
-        -------
-
-        """
-        # sanitise input
-        dim = str(dim.strip().lower())
-
-        # dim to index for 3d or less
-        dim_to_index = {'x': 0,
-                        'y': 1,
-                        'z': 2}
-
-        dim_idx = dim_to_index[dim]
-
-        # check and correct index for higher dimensionality
-        if self.ndim > 3:
-            dim_idx = -dim_idx - 1
-
-        return dim_idx
-
-    def _get_dim_at_spatial_index(self, idx: int):
-        return self.data[:, idx]
-
-    def _get_named_dimension(self, dim: str, as_type='array'):
+    def _get_named_dimensions(self, dim, as_type='array'):
         """
         Get data for a named dimension or multiple named dimensions of the object
 
@@ -91,41 +73,34 @@ class PointBlock(SimpleBlock):
         if as_type not in ('array', 'tuple'):
             raise ValueError("Argument 'as_type' must be a string from 'array' or 'tuple'")
 
-        if len(dim) > 1:
-            # split dims up and get each separately
-            data = [self._get_named_dimension(_dim) for _dim in dim]
+        dim = list(dim)
+        data = self.data.sel(spatial=dim)
 
-            # decide on output type and return array or tuple as requested, default to array
-            if as_type == 'array':
-                return np.column_stack(data)
-            elif as_type == 'tuple':
-                return tuple(data)
-
-        else:
-            # get index of named dimension along spatial axis
-            dim_idx = self._named_dimension_to_spatial_index(dim)
-            # index into self to get data
-            return self._get_dim_at_spatial_index(dim_idx)
+        # decide on output type and return array or tuple as requested, default to array
+        if as_type == 'array':
+            return data
+        elif as_type == 'tuple':
+            return tuple(data.T)
 
     @property
     def x(self):
-        return self._get_named_dimension('x')
+        return self._get_named_dimensions('x')
 
     @property
     def y(self):
-        return self._get_named_dimension('y')
+        return self._get_named_dimensions('y')
 
     @property
     def z(self):
-        return self._get_named_dimension('z')
+        return self._get_named_dimensions('z')
 
     @property
     def xyz(self):
-        return self._get_named_dimension('xyz')
+        return self._get_named_dimensions('xyz')
 
     @property
     def zyx(self):
-        return self._get_named_dimension('zyx')
+        return self._get_named_dimensions('zyx')
 
     def as_zyx(self):
         """

peepingtom/io_/read/star/reader_functions.py
@@ -1,7 +1,7 @@
 """This module contains various reader functions for parsing data from different types of STAR
 files.
 
-Each function should attempt to raise a ValueError quickly if it's not the right function
+Each function should attempt to raise a ParseError quickly if it's not the right function
 for reading a given file.
 
 raw data is the dictionary produced by starfile.read()
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import eulerangles
 
-from ...utils import guess_name
+from ...utils import guess_name, ParseError
 from ....datablocks import ParticleBlock
 
 
@@ -33,7 +33,10 @@ shift_headings = {
     }
 }
 
-pixel_size_headings = ['rlnImagePixelSize']
+pixel_size_headings = {
+    '3.0': ['rlnPixelSize'],
+    '3.1': ['rlnImagePixelSize']
+}
 micrograph_name_heading = 'rlnMicrographName'
 
 
@@ -49,7 +52,7 @@ def extract_data(df, mode='3.1', name_regex=None):
 
         coords = df_volume[coord_headings[dim]].to_numpy(dtype=float)
         shifts = df_volume.get(shift_headings[dim][mode], pd.Series([0.0])).to_numpy()
-        px_size = df_volume.get(pixel_size_headings, pd.Series([1.0])).to_numpy()
+        px_size = df_volume.get(pixel_size_headings[mode], pd.Series([1.0])).to_numpy()
         shifts = shifts / px_size
         coords += shifts
 
@@ -61,7 +64,13 @@ def extract_data(df, mode='3.1', name_regex=None):
 
         properties = {key: df_volume[key].to_numpy() for key in df.columns}
 
-        particleblocks.append(ParticleBlock(coords, rotation_matrices, properties, name=name))
+        # TODO: better way to handle pizel size? Now we can only account for uniform size
+        pixel_size = px_size.flatten()[0]
+        if dim == '3d':
+            pixel_size = [pixel_size] * 3
+        else:
+            pixel_size = [pixel_size] * 2
+        particleblocks.append(ParticleBlock(coords, rotation_matrices, properties, pixel_size=np.array(pixel_size), name=name))
 
     return particleblocks
 
@@ -96,7 +105,7 @@ def parse_relion30(raw_data, **kwargs):
     """Attempt to parse raw data dict from starfile.read as a RELION 3.0 style star file
     """
     if len(raw_data.values()) > 1:
-        raise ValueError("Cannot parse as RELION 3.0 format STAR file")
+        raise ParseError("Cannot parse as RELION 3.0 format STAR file")
 
     df = list(raw_data.values())[0]
     return extract_data(df, mode='3.0', **kwargs)
@@ -106,7 +115,7 @@ def parse_relion31(raw_data, **kwargs):
     """Attempt to parse raw data from starfile.read as a RELION 3.1 style star file
     """
     if list(raw_data.keys()) != ['optics', 'particles']:
-        raise ValueError("Cannot parse as RELION 3.1 format STAR file")
+        raise ParseError("Cannot parse as RELION 3.1 format STAR file")
 
     df = raw_data['particles'].merge(raw_data['optics'])
     return extract_data(df, mode='3.1', **kwargs)