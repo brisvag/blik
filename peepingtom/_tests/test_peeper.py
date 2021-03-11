c7914687eec45c92d5157bc8dc714aa209301e88
Lorenzo Gaifas <brisvag@gmail.com>
2021-03-11 13:22:14 +0100

    added peeper tests

---
+++ peepingtom/_tests/test_peeper.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+
+from peepingtom.peeper import Peeper
+from peepingtom.datablocks import PointBlock, ImageBlock
+from peepingtom.depictors.viewer import Viewer
+
+
+pb = PointBlock(volume='test')
+ib = ImageBlock(np.random.rand(10, 10, 10), volume='test')
+peeper = Peeper([pb, ib])
+
+
+def test_peeper():
+    assert len(peeper) == 2
+    assert len(peeper.datablocks) == 2
+    assert len(peeper.volumes) == 1
+
+
+def test_sanitize_peeper():
+    with pytest.raises(TypeError):
+        peeper.append('wrong')
+
+
+def test_view():
+    view = peeper[:1]
+    assert len(view) == 1
+    assert view._parent is peeper
+    with pytest.raises(TypeError):
+        view.append(pb)
+
+
+def test_pprint():
+    brep = peeper.__pretty_repr__('base')
+    frep = peeper.__pretty_repr__('full')
+    assert len(brep.split('\n')) == 1
+    assert len(frep.split('\n')) == 4
+
+
+def test_depict():
+    peeper.datablocks.depict()
+    assert len(peeper.napari_layers) == 2
