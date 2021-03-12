aa321180fde868c0715fc47c241ec89d834233bc
Alister Burt <alisterburt@gmail.com>
2021-03-12 20:27:51 +0000

    working delayed interface

--- peepingtom/datablocks/simpleblocks/simpleblock.py
+++ peepingtom/datablocks/simpleblocks/simpleblock.py
@@ -1,5 +1,5 @@
-from xarray import DataArray
 from dask import delayed
+from xarray import DataArray
 
 from ..datablock import DataBlock
 
@@ -14,18 +14,17 @@ class SimpleBlock(DataBlock):
 
     Calling __getitem__ on a SimpleBlock will call __getitem__ on its data property and return a view
     """
-    def __init__(self, data=(), delayed=False, **kwargs):
+
+    def __init__(self, data=(), reader_function=None, **kwargs):
         super().__init__(**kwargs)
+        self.reader_function = reader_function
         self.data = data
-        self._delayed = delayed
 
     @property
     def data(self):
-        if self._delayed:
-            data = self._data_setter(self._data.compute())
-        else:
-            data = self._data
-        return data
+        if callable(self._data):
+            return self._data.compute()
+        return self._data
 
     @data.setter
     def data(self, data):
@@ -33,8 +32,8 @@ class SimpleBlock(DataBlock):
             self._data = data.data
         elif isinstance(data, DataArray):
             self._data = data
-        elif callable(data):
-            self._data = delayed(data)
+        elif callable(self._reader_function):
+            self._data = self._reader_function(data)
         else:
             self._data = self._data_setter(data)
         self.update()
@@ -44,6 +43,17 @@ class SimpleBlock(DataBlock):
         takes raw data and returns it properly formatted to the SimpleBlock subclass specification.
         """
         raise NotImplementedError('SimpleBlocks must implement this method')
+
+    @property
+    def reader_function(self):
+        return self._reader_function
+
+    @reader_function.setter
+    def reader_function(self, function):
+        if function is None:
+            self._reader_function = function
+        else:
+            self._reader_function = delayed(function)
 
     def __setitem__(self, key, value):
         self.data[key] = value
