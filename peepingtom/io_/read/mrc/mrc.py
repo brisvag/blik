ec3388c71ece9d4b0f43d0482ba2e16b69df13a4
Alister Burt <alisterburt@gmail.com>
2021-03-18 09:54:25 +0000

    if else

--- peepingtom/io_/read/mrc/mrc.py
+++ peepingtom/io_/read/mrc/mrc.py
@@ -5,11 +5,14 @@ from ...utils import guess_name
 from numpy.lib.recfunctions import structured_to_unstructured
 
 
-def read_mrc(image_path, name_regex=None, **kwargs):
+def read_mrc(image_path, name_regex=None, mmap=True, **kwargs):
     """
     read an mrc file and return an ImageBlock
     """
     name = guess_name(image_path, name_regex)
-    mrc = mrcfile.mmap(image_path)
+    if mmap is True:
+        mrc = mrcfile.mmap(image_path)
+    else:
+        mrc = mrcfile.open(image_path)
     pixel_size = structured_to_unstructured(mrc.voxel_size)[::-1]
     return ImageBlock(mrc.data, pixel_size=pixel_size, name=name)
