64021fe75e686b8a141436da6842e32b7a1bca31
Lorenzo Gaifas <brisvag@gmail.com>
2021-05-19 13:13:19 +0200

    updated napari version and reimplemented gui test

--- peepingtom/gui/_tests/test_napari.py
+++ peepingtom/gui/_tests/test_napari.py
@@ -1,9 +1,8 @@
-# from peepingtom.peeper import Peeper
+from peepingtom.peeper import Peeper
 
 
-# TODO widget leaks break this test. Wait for `strict_qt` arg to be merged in napari
-# def test_napari(make_napari_viewer):
-    # p = Peeper()
-    # viewer = make_napari_viewer(strict=False)
-    # p.show(napari_viewer=viewer)
-    # p.purge_gui()
+def test_napari(make_napari_viewer):
+    p = Peeper()
+    viewer = make_napari_viewer(strict_qt=False)
+    p.show(napari_viewer=viewer)
+    p.purge_gui()
