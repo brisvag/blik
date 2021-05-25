1d65926aede842a8d59aa4eae5dd3754e42b5e62
Lorenzo Gaifas <brisvag@gmail.com>
2021-05-25 14:02:49 +0200

    expanded tests for viewer, fixed issue with kwargs

--- peepingtom/gui/_tests/test_napari.py
+++ peepingtom/gui/_tests/test_napari.py
@@ -1,8 +1,10 @@
 from peepingtom.peeper import Peeper
+from peepingtom.datablocks import PointBlock
 
 
 def test_napari(make_napari_viewer):
-    p = Peeper()
+    p = Peeper(PointBlock(data=[[0, 0, 0]]))
     viewer = make_napari_viewer(strict_qt=False)
     p.show(napari_viewer=viewer)
+    p.viewer.toggle_plots()
     p.purge_gui()
