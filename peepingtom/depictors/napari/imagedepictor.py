1b63ccbc6e2e5c835098ae43a830cd2fa09d9e0b
Lorenzo Gaifas <brisvag@gmail.com>
2021-06-03 15:32:02 +0200

    handle scaling based on pixel size on the peepingtom side

--- peepingtom/depictors/napari/imagedepictor.py
+++ peepingtom/depictors/napari/imagedepictor.py
@@ -3,4 +3,6 @@ from .naparidepictor import NapariDepictor
 
 class ImageDepictor(NapariDepictor):
     def depict(self):
-        self._make_image_layer(self.datablock.data, name=f'{self.name} - image', scale=self.datablock.pixel_size)
+        self._make_image_layer(self.datablock.data,
+                               name=f'{self.name} - image',
+                               scale=self.datablock.pixel_size)
