c6d0f66838940985e255b5fe5629821914e38384
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-11 12:43:17 +0100

    read stack files

--- blik/_plugin/reader.py
+++ blik/_plugin/reader.py
@@ -59,7 +59,8 @@ def read_image(image):
         dict(
             name=f'{image.name} - image',
             scale=image.pixel_size,
-            metadata={'volume': image.name}
+            metadata={'volume': image.name},
+            interpolation='spline36',
         ),
         'image',
     )
