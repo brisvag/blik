8dc0d95a0b6ffb578d706f58aff36e72a0179f17
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-18 10:42:39 +0100

    fix test

--- blik/_tests/test_widget.py
+++ blik/_tests/test_widget.py
@@ -5,9 +5,9 @@ import napari
 def test_volume_widget(make_napari_viewer):
     viewer = make_napari_viewer()
 
-    wdg = volume_selector()
-    viewer.window.add_dock_widget(wdg)
-
     with napari.layers._source.layer_source(reader_plugin='blik'):
         viewer.add_points(metadata={'volume': 'test'})
+
+    wdg = volume_selector()
+    viewer.window.add_dock_widget(wdg)
     assert wdg.volume.choices == ('test',)
