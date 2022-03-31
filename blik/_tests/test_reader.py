4172a748cde0df83e036a8c26cfe56c48d371731
Lorenzo Gaifas <brisvag@gmail.com>
2022-03-31 17:04:03 +0200

    add some tests

--- blik/_tests/test_reader.py
+++ blik/_tests/test_reader.py
@@ -1,29 +1,11 @@
-import pandas as pd
-import starfile
 from blik.reader import get_reader
 
 
-def test_reader(tmp_path):
-    df = pd.DataFrame({
-        'rlnCoordinateX': [1, 1],
-        'rlnCoordinateY': [2, 2],
-        'rlnCoordinateZ': [3, 3],
-        'rlnOriginX': [0.1, 0.1],
-        'rlnOriginY': [0.2, 0.2],
-        'rlnOriginZ': [0.3, 0.3],
-        'rlnAngleRot': [0, 0],
-        'rlnAngleTilt': [0, 90],
-        'rlnAnglePsi': [90, 0],
-        'rlnMicrographName': ['a', 'b'],
-        'feature': ['x', 'y'],
-    })
-    file_path = tmp_path / 'test.star'
-    starfile.write(df, file_path)
-
-    reader = get_reader(file_path)
+def test_reader(star_file):
+    reader = get_reader(star_file)
     assert callable(reader)
 
-    layer_data_list = reader(file_path)
+    layer_data_list = reader(star_file)
     assert len(layer_data_list) == 8
 
     pts = layer_data_list[0]
