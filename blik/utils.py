57024f4ad7b21300a4a2a8af8dfa5b00f92553ec
Lorenzo Gaifas <brisvag@gmail.com>
2023-02-21 14:37:58 +0100

    fix vector generation

--- blik/utils.py
+++ blik/utils.py
@@ -1,11 +1,11 @@
-import numpy as np
 import einops
+import numpy as np
 from scipy.spatial.transform import Rotation
 
 
 def generate_vectors(coords, orientations):
     mat = Rotation.concatenate(orientations).as_matrix()
-    basis_vecs = einops.rearrange(mat, 'batch a b -> b batch a')
+    basis_vecs = einops.rearrange(mat, "batch a b -> b batch a")
     vec_data = np.empty((len(coords) * 3, 2, 3))
     vec_color = np.empty((len(coords) * 3, 3))
     for idx, vecs in enumerate(basis_vecs):
@@ -14,4 +14,3 @@ def generate_vectors(coords, orientations):
         vec_data[idx::3] = np.stack([coords, vecs], axis=1)
         vec_color[idx::3] = color
     return vec_data, vec_color
-
