04e7dd76dff6fb545530f67fe19ca0e2f49483fa
Lorenzo Gaifas <brisvag@gmail.com>
2023-03-09 14:52:37 +0100

    fix tests

--- blik/_tests/test_reader.py
+++ blik/_tests/test_reader.py
@@ -8,7 +8,7 @@ def test_reader(star_file):
     layer_data_list = reader(star_file)
     assert len(layer_data_list) == 4
 
-    pts = layer_data_list[0]
-    assert pts[2] == 'points'
-    vec = layer_data_list[1]
-    assert vec[2] == 'vectors'
+    pts = layer_data_list[1]
+    assert pts[2] == "points"
+    vec = layer_data_list[0]
+    assert vec[2] == "vectors"
--- blik/widgets/filter.py
+++ blik/widgets/filter.py
@@ -1,8 +1,8 @@
 from typing import TYPE_CHECKING
 
+import numpy as np
 from magicgui import magic_factory
 from skimage.filters import butterworth, gaussian
-import numpy as np
 
 if TYPE_CHECKING:
     import napari
@@ -11,22 +11,28 @@ if TYPE_CHECKING:
 @magic_factory(
     auto_call=False,
     call_button=True,
-    low=dict(widget_type='FloatSlider', min=0, max=1),
-    high=dict(widget_type='FloatSlider', min=0, max=1),
+    low=dict(widget_type="FloatSlider", min=0, max=0.5),
+    high=dict(widget_type="FloatSlider", min=0, max=0.5),
 )
-def bandpass_filter(image: 'napari.layers.Image', low: float = 0.1, high: float = 0.7) -> 'napari.types.LayerDataTuple':
-    channel_axis = 0 if image.metadata['stack'] else None
-    high_pass = butterworth(np.asarray(image.data), low, high_pass=True, channel_axis=channel_axis)
+def bandpass_filter(
+    image: "napari.layers.Image", low: float = 0.1, high: float = 0.4
+) -> "napari.types.LayerDataTuple":
+    channel_axis = 0 if image.metadata["stack"] else None
+    high_pass = butterworth(
+        np.asarray(image.data), low, high_pass=True, channel_axis=channel_axis
+    )
     low_pass = butterworth(high_pass, high, channel_axis=channel_axis)
-    return low_pass, dict(name=f'filtered {image.name}', scale=image.scale), 'image'
+    return low_pass, dict(name=f"filtered {image.name}", scale=image.scale), "image"
 
 
 @magic_factory(
     auto_call=False,
     call_button=True,
-    sigma=dict(widget_type='FloatSlider', min=0, max=10),
+    sigma=dict(widget_type="FloatSlider", min=0, max=10),
 )
-def gaussian_filter(image: 'napari.layers.Image', sigma: float = 1) -> 'napari.types.LayerDataTuple':
-    channel_axis = 0 if image.metadata['stack'] else None
+def gaussian_filter(
+    image: "napari.layers.Image", sigma: float = 1
+) -> "napari.types.LayerDataTuple":
+    channel_axis = 0 if image.metadata["stack"] else None
     filtered = gaussian(np.asarray(image.data), sigma=sigma, channel_axis=channel_axis)
-    return filtered, dict(name=f'filtered {image.name}', scale=image.scale), 'image'
+    return filtered, dict(name=f"filtered {image.name}", scale=image.scale), "image"
