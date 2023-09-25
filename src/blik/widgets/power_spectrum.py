b85c86f4005884d20ae9c06d1019fc1a162fd2a9
Lorenzo Gaifas <brisvag@gmail.com>
2023-09-25 17:40:57 +0200

    fix power spectrum widget (log, properly use axes)

--- src/blik/widgets/power_spectrum.py
+++ src/blik/widgets/power_spectrum.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 import dask.array as da
+import numpy as np
 from magicgui import magic_factory
 from scipy.fft import fftn, fftshift
 
@@ -15,11 +16,18 @@ if TYPE_CHECKING:
 def power_spectrum(
     image: "napari.layers.Image",
 ) -> "napari.types.LayerDataTuple":
+    """
+    Power spectrum (log scale) of the image.
+
+    First centers in real space on the origin to remove shift effects.
+    """
     axes = (1, 2) if image.metadata["stack"] else None
     raw = da.compute(image.data)[0]
-    power_spectrum = abs(fftshift(fftn(raw, axes=axes)))
+    power_spectrum = np.abs(
+        fftshift(fftn(fftshift(raw, axes=axes), axes=axes), axes=axes)
+    )
     return (
-        power_spectrum,
+        np.log(power_spectrum + 1),
         {"name": f"{image.name} - power spectrum", "scale": image.scale},
         "image",
     )
