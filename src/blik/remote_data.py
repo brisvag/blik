ca79233374c792ae475e3be4df0173ae9f93a357
Lorenzo Gaifas <brisvag@gmail.com>
2024-07-15 12:48:27 +0200

    use new zenodo entry

--- src/blik/remote_data.py
+++ src/blik/remote_data.py
@@ -4,11 +4,22 @@ from .reader import read_layers
 
 
 def load_hiv_dataset():
-    reg = pooch.create(
+    reg_tomo = pooch.create(
         path=pooch.os_cache("blik"),
-        base_url='doi:10.5281/zenodo.6504891/',
+        base_url="doi:10.5281/zenodo.6504891/",
     )
-    reg.load_registry_from_doi()
+    reg_tomo.load_registry_from_doi()
 
-    paths = [reg.fetch(f) for f in reg.registry_files]
+    # made separate zenodo entry for star file with pixel size
+    reg_picks = pooch.create(
+        path=pooch.os_cache("blik"),
+        base_url="doi:10.5281/zenodo.12743309/",
+    )
+    reg_picks.load_registry_from_doi()
+
+    paths = [
+        reg_tomo.fetch("01_10.00Apx.mrc"),
+        reg_picks.fetch("01_10.00Apx.star"),
+    ]
+
     return read_layers(*paths)
